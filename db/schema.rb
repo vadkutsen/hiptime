466f74a1da56a7f74839f429011a6d85757e7cb4
vadkutsen <vadkutsen@gmail.com>
2018-07-02 01:51:24 +0200

    Add user_id to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_30_014028) do
+ActiveRecord::Schema.define(version: 2018_07_01_234041) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
